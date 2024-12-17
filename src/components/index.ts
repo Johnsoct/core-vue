a785561d08d071f70fda82a4262f7c6f30eaed1c
Taylor Johnson <johnsoctbus@gmail.com>
2024-12-17 16:22:07 -0500

    Fixed tags being imported and exported in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,9 @@
 // Base
 import BaseButton from './base/base-button.vue';
 import BaseInput from './base/base-input.vue';
-import BaseInvoiceTag from './base/base-invoice-tag.vue';
 import BaseModal from './base/base-modal.vue';
 import BaseRadioInput from './base/base-radio-input.vue';
-import BaseReservesTag from './base/base-reserves-tag.vue';
 import BaseSelect from './base/base-select.vue';
-import BaseTransfersTag from './base/base-transfers-tag.vue';
 
 // SVG
 import AlertCircle from './svg/alert-circle.vue';
@@ -21,22 +18,19 @@ import Stars from './svg/stars.vue';
 import XClose from './svg/x-close.vue';
 
 export {
-  AlertCircle,
-  AlertTriangle,
-  BaseButton,
-  BaseInput,
-  BaseInvoiceTag,
-  BaseModal,
-  BaseRadioInput,
-  BaseReservesTag,
-  BaseSelect,
-  BaseTransfersTag,
-  CheckCircle,
-  Ellipsis,
-  Eye,
-  LightningBolt,
-  Message,
-  Ring,
-  Stars,
-  XClose,
+        AlertCircle,
+        AlertTriangle,
+        BaseButton,
+        BaseInput,
+        BaseModal,
+        BaseRadioInput,
+        BaseSelect,
+        CheckCircle,
+        Ellipsis,
+        Eye,
+        LightningBolt,
+        Message,
+        Ring,
+        Stars,
+        XClose,
 };
