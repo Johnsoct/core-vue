2a3fc17ee4b80cdc3a099e77bac37075b245e7e3
Taylor Johnson <johnsoct@hey.com>
2025-02-01 10:29:30 -0500

    wip linting updates?

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // Packages
 import {
-  type App,
-  createApp,
+    type App,
+    createApp,
 } from 'vue';
 // Components
 import * as components from '@src/components';
@@ -14,23 +14,23 @@ import '@src/styles/main.scss';
 const app = createApp(Styleguide);
 
 app.use(ToastrPlugin, {
-  shareAppContext: true,
+    shareAppContext: true,
 });
 
 app.mount('#styleguide');
 
 // Exports the entire library as a plugin, which allows it to be installed via app.use()
 const CoreFrontend = {
-  install: (app: App) => {
-    for (const key in components) {
-      // @ts-expect-error - There is nothing wrong with this statement, and it works
-      app.component(key, components[key]);
-    }
+    install: (app: App) => {
+        for (const key in components) {
+            // @ts-expect-error - There is nothing wrong with this statement, and it works
+            app.component(key, components[key]);
+        }
 
-    app.use(ToastrPlugin, {
-      shareAppContext: true,
-    });
-  },
+        app.use(ToastrPlugin, {
+            shareAppContext: true,
+        });
+    },
 };
 
 export default CoreFrontend;
